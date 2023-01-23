3e90aacbf2c8bd9c3502600d7e442f7100193db6
Mark Archer <marka@ifit.com>
2023-01-23 09:35:40 -0800

    chore: Remove unneeded code

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,5 @@
 import _ from 'lodash';
 
-export type FetchMethods = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
-
-export function PushNotifications_urlBase64ToUint8Array(base64String: string): Uint8Array {
-  const padding = '='.repeat((4 - base64String.length % 4) % 4);
-  const base64 = (base64String + padding)
-    .replace(/-/g, '+')
-    .replace(/_/g, '/');
- 
-  const rawData = window.atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
- 
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
-export function htmlToText(html: string): string {
-  let doc = new DOMParser().parseFromString(html, 'text/html');
-  return doc.body.textContent || "";
-}
-
 export type IObjectMatch<T> = {
   [key in keyof T]?: RegExp | string | number | boolean
 }
@@ -58,14 +36,6 @@ export function camelCaseToHyphens(s: string) {
   return s.split(' ').join('-').toLowerCase();
 }
 
-export function cloneClean(obj: any) {
-  return JSON.parse(JSON.stringify(obj));
-}
-
-export function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 export const moneyFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
@@ -84,15 +54,6 @@ export function formatMoney(value: number, precision: number = 2) {
     maximumFractionDigits: precision,
   });
   return _moneyFormatter.format(value)
-}
-
-export function parseMoney(strAmount: string) {
-  strAmount = strAmount.trim();
-  let isNegative = strAmount.startsWith("(");
-  strAmount = strAmount.replace("(", "").replace(")", "");
-  let amount = Number(strAmount.replace(/[^0-9.-]+/g,""));
-  amount = amount * (isNegative ? -1 : 1);
-  return amount;
 }
 
 // @ts-ignore
