823a113c7a274cbc5f8b2def1a5b20b6bde59f4f
Mark Archer <marka@ifit.com>
2023-07-02 16:40:10 -0700

    feat: Use compiled source

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+export * from "./kore/autogrid"
+export * from "./kore/autoscreen-grid"
+export * from "./kore/datagrid"
+export * from "./kore/input-number"
+export * from "./kore/input"
+export * from "./kore/lazy-list"
+export * from "./kore/lazy-sortable-list"
+export * from "./kore/sortable-list"
+export * from "./kore/typeahead-fk"
+export * from "./kore/typeahead"
+export * from "./orm"
+
+export * as hooks from "./kore/kore-hooks"
+export * as orm from "./orm"
